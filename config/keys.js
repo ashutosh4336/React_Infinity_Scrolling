2f7407ad0f9e9e918d7bdeb48894333261208163
Ashutosh Panda <ashu.juga99@gmail.com>
2019-02-20 02:05:13 +0530

    Unsplash API integrated on Backend

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  APPLICATION_ID:
-    process.env.APPLICATION_ID ||
-    "e9c62bd7b72a2fc73d9b7241fc0153084e737d2081dc250663c0a33b6bd44247",
-  SECRET:
-    process.env.SECRET ||
-    "cf4f7eba964fbfaf1728e3c6a8ae67d4e6b77e7446e9d93735bb1f809cb88884",
-  CALLBACK_URL: process.env.CALLBACK_URL || "http://127.0.0.1:3000"
-};
